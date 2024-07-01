97dc055924830024f60abd258c0411cb012c0589
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-07-01 22:04:13 +0000

    se ha realizado el footer

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -9,17 +9,16 @@ const cardStyles = {
 
 export const Cards = (props) => {
     return (
-        <div className="card mb-4" style={cardStyles}>
-            <img src={props.img} class="card-img-top" alt="" />
-            <div class="card-body d-flex flex-column">
-                <h5 class="card-title">{props.cardTitle}</h5>
-                <p class="card-text">{props.cardText}</p>
+            <div className="card mb-4" style={cardStyles}>
+                <img src={props.img} class="card-img-top" alt="destinos turisticos" style={{"height":"200px"}} />
+                <div class="card-body d-flex flex-column" style={{"height":"190px"}}>
+                    <h5 class="card-title">{props.cardTitle}</h5>
+                    <p class="card-text">{props.cardText}</p>
+                </div>
+                <div className="border-top border-1 p-3 d-flex justify-content-center">
+                    <a href="#" class="btn btn-primary w-40 align-self-center">Find out More!</a>
+                </div>
             </div>
-            <div className="border-top border-1 p-3 d-flex justify-content-center">
-                <a href="#" class="btn btn-primary w-40 align-self-center">Find out More!</a>
-            </div>
-
-        </div>
     );
 };
 
--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 
 export const Footer = () => {
     return (
-        <div className='container-fluid bg-dark h-50 '>
-            <p className='text-light p-0'>Copyright &copy; Todos los derechos reservados 2024</p>
-        </div>
+            <div className='container d-flex justify-content-center align-items-center h-100'>
+                   <p className='text-light p-0 m-0'>Copyright &copy; Todos los derechos reservados 2024</p>
+            </div>
+         
 
     );
 }
