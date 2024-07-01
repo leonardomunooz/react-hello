b8c5a71205ae6e4232f8a2b1dfeb3f523c822bf7
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-07-01 22:07:55 +0000

    se ha modificado el navbar

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 export const Navbar = (props) => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-b navbar-light bg-dark  ">
+        <nav className="navbar navbar-expand-lg navbar-b navbar-dark bg-dark  ">
             <div className="container d-flex justify-content-between justify-md-content-between">
                 <div className="d-flex justify-content-between w-100">
             
